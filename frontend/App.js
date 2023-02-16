901731d69c263c27839078c5fd36851f60bab338
Vittorio Lo Mele <vittorio@mrbackslash.it>
2023-02-16 13:48:27 +0100

    ant design

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from '@ant-design/react-native/lib/button';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hivelink</Text>
       <StatusBar style="auto" />
+      <Button>Vafangul</Button>
     </View>
   );
 }
