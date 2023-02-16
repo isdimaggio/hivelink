5445f9d18915ce9e9d838bb85ed2010ac8d9879b
massamt <massamariateresa4@gmail.com>
2023-02-16 16:49:05 +0100

    username e password
    minimal giusto per provare

--- frontend/App.js
+++ frontend/App.js
@@ -1,16 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Button from '@ant-design/react-native/lib/button';
+import InputItem from '@ant-design/react-native/lib/input-item';
+import { useState } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hivelink</Text>
-      <StatusBar style="auto" />
-      <Button>Vafangul</Button>
-    </View>
-  );
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    return (
+        <View style={styles.container}>
+            <Text>Hivelink</Text>
+            <StatusBar style="auto" />
+            <Button>Vafangul</Button>
+            <InputItem
+                clear
+                value={username}
+                onChange={(value: string) => {
+                    setUsername(value);
+                }}
+                placeholder="inzacca l'username"
+            />
+            <InputItem
+                clear
+                type="password"
+                value={password}
+                onChange={(value: string) => {
+                    setPassword(value);
+                }}
+                placeholder="passwa"
+            />
+        </View>
+    );
 }
+
 
 const styles = StyleSheet.create({
   container: {
