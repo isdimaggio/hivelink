303ffd5b13eee363c6ac21e21658dadffa9e1fec
massamt <massamariateresa4@gmail.com>
2023-02-17 12:06:06 +0100

    2 schermate
    ado mo abbiamo pure due schermatew

--- frontend/App.js
+++ frontend/App.js
@@ -1,4 +1,67 @@
-import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+
+const SecondaSchermata = ({navigation}) => {
+    return(
+        <View style = {styles.container}>
+            <Button
+                title = "Home"
+                onPress={() => navigation.navigate('Home')}
+            />
+        </View>
+    )
+}
+
+const Home = ({navigation}) => {
+    return(
+        <View style = {styles.container}>
+            <Button
+                title = "seconda schermata"
+                onPress={() => navigation.navigate('SecondaSchermata')}
+            />
+        </View>
+    )
+}
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name = "Home" component = {Home}/>
+                <Stack.Screen name = "SecondaSchermata" component = {SecondaSchermata}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+    },
+    input: {
+        width: '80%',
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
+
+/*import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
@@ -67,5 +130,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default HomeScreen;
-
+export default HomeScreen;*/
