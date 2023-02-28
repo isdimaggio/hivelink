90e3f85298776b1fe001ee1bbdc94f40dc6d34bb
massamt <massamariateresa4@gmail.com>
2023-02-28 13:00:52 +0100

    bho tabellina
    bozza

--- frontend/Home.js
+++ frontend/Home.js
@@ -1,5 +1,24 @@
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
+const styles = StyleSheet.create({
+    tableRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        marginBottom: 1,
+    },
+    tableCell: {
+        flex: 1,
+        height: 50,
+        borderWidth: 1,
+        borderColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
+
+
  function Home({navigation}) {
 
     const NavigateGraph = () => {
@@ -8,7 +27,36 @@ import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
     return(
         <View>
-            <Text>Sei nella Home</Text>  
+            <View style={styles.tableRow}>
+                <View style={styles.tableCell}><Text>1</Text></View>
+                <View style={styles.tableCell}><Text>2</Text></View>
+                <View style={styles.tableCell}><Text>3</Text></View>
+                <View style={styles.tableCell}><Text>4</Text></View>
+                <View style={styles.tableCell}><Text>5</Text></View>
+            </View>
+            <View style={styles.tableRow}>
+                <View style={styles.tableCell}><Text>6</Text></View>
+                <View style={styles.tableCell}><Text>7</Text></View>
+                <View style={styles.tableCell}><Text>8</Text></View>
+                <View style={styles.tableCell}><Text>9</Text></View>
+                <View style={styles.tableCell}><Text>10</Text></View>
+            </View>
+            <View style={styles.tableRow}>
+                <View style={styles.tableCell}><Text>11</Text></View>
+                <View style={styles.tableCell}><Text>12</Text></View>
+                <View style={styles.tableCell}><Text>13</Text></View>
+                <View style={styles.tableCell}><Text>14</Text></View>
+                <View style={styles.tableCell}><Text>15</Text></View>
+            </View>
+            <View style={styles.tableRow}>
+                <View style={styles.tableCell}><Text>11</Text></View>
+                <View style={styles.tableCell}><Text>12</Text></View>
+                <View style={styles.tableCell}><Text>13</Text></View>
+                <View style={styles.tableCell}><Text>14</Text></View>
+                <View style={styles.tableCell}><Text>15</Text></View>
+            </View>
+
+            <Text>Sei nella Home</Text>
             <Button
             title = "grafici"
             onPress = {NavigateGraph}
