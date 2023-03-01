3a3bc40150d2b11a06365aa3c59d70f7bc7b5fcc
tonyRitrovato <115578428+tonyRitrovato@users.noreply.github.com>
2023-03-01 10:59:40 +0100

    chart periodic render

--- frontend/Graph.js
+++ frontend/Graph.js
@@ -1,13 +1,19 @@
-import { StyleSheet, View } from 'react-native';
-import { VictoryLine, VictoryChart, VictoryTheme } from "victory-native";
+import { StyleSheet, View, Text } from 'react-native';
+import { VictoryLine, VictoryChart } from "victory-native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {useEffect, useState} from 'react';
 
   const Tab = createBottomTabNavigator();
 
 function Graph () {
 
     return(
-      <Tab.Navigator>
+      <Tab.Navigator
+      screenOptions={() => ({
+        tabBarInactiveTintColor: 'gray',
+        tabBarActiveTintColor: 'tomato',
+      })}
+      >
         <Tab.Screen name="Temperatura" component={Temperature} />
         <Tab.Screen name="Umidità" component={Umidity} />
         <Tab.Screen name="Peso" component={Weight} />
@@ -18,14 +24,26 @@ function Graph () {
 const Weight = () => {
   let data = [];
   for(let i = 0; i < 10; i++){
-    data.push({ora: i, temperatura: i + 2});
+    data.push({date: i, weight: i + 2});
   }
+
+  const [update, setUpdate] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setUpdate(data[data.length - 1]);
+    }, 1000); // aggiorna il tempo ogni secondo
+
+    return () => clearInterval(interval);
+  }, [update]);
 
     return(
         <View style={styles.container}>
-        <VictoryChart width={350} theme={VictoryTheme.material}>
-          <VictoryLine data={data} x="ora" y="temperatura" />
+        <VictoryChart width={350}>
+          <VictoryLine data={data} x="date" y="weight" />
         </VictoryChart>
+        <Text>Ultimo aggiornamento: {update.date}</Text>
+        <Text>Peso: {update.weight}</Text>
       </View>
     );
 }
@@ -33,14 +51,26 @@ const Weight = () => {
 const Umidity = () => {
   let data = [];
   for(let i = 0; i < 10; i++){
-    data.push({ora: i, temperatura: i});
+    data.push({date: i, umidity: i});
   }
+
+  const [update, setUpdate] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setUpdate(data[data.length - 1]);
+    }, 1000); // aggiorna il tempo ogni secondo
+
+    return () => clearInterval(interval);
+  }, [update]);
 
     return(
         <View style={styles.container}>
-        <VictoryChart width={350} theme={VictoryTheme.material}>
-          <VictoryLine data={data} x="ora" y="temperatura" />
+        <VictoryChart width={350}>
+          <VictoryLine data={data} x="date" y="umidity" />
         </VictoryChart>
+        <Text>ultimo aggiornamento: {update.date} </Text>
+        <Text>Umidità: {update.umidity}</Text>
       </View>
     );
 }
@@ -49,17 +79,29 @@ const Temperature = () => {
 
   let data = [];
   for(let i = 0; i < 10; i++){
-    data.push({ora: i, temperatura: i*i});
+    data.push({date: i, temperature: i*i});
   }
+
+  const [update, setUpdate] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setUpdate(data[data.length - 1]);
+    }, 1000); // aggiorna il tempo ogni secondo
+
+    return () => clearInterval(interval);
+  }, [update]);
 
     return(
         <View style={styles.container}>
-        <VictoryChart width={350} theme={VictoryTheme.material}>
-          <VictoryLine data={data} x="ora" y="temperatura" />
+        <VictoryChart width={350}>
+          <VictoryLine data={data} x="date" y="temperature" />
         </VictoryChart>
+        <Text>ultimo aggiornamento avvenuto: {update.date}</Text>
+        <Text>temperatura: {update.temperature}</Text>
       </View>
     );
-}
+  }
 
 const styles = StyleSheet.create({
     container: {
