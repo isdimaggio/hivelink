2b5a6bb6cf4ef9468f7716ae2d98f6eb636ae543
massamt <massamariateresa4@gmail.com>
2023-03-03 09:44:31 +0100

    Update Home.js
    tabella quasi finita, tutto funzionante ma sovrapposto

--- frontend/Home.js
+++ frontend/Home.js
@@ -30,64 +30,80 @@ import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-ta
 
 
     // problema da risolvere: dopo la prima riga non viene mostrata
+
      const tableData = [
-         ['1', '2', '3', '4', <Button
-             title = "dettagli"
-             onPress = {NavigateGraph}
-         />],
-         ['1', '2', '3', '4', '5'],
-         ['1', '2', '3', '4', '5'],
-         ['1', '2', '3', '4', '5'],
-         ['1', '2', '3', '4', '5']
+         [<Button title = "dettagli" onPress = {NavigateGraph} />],
+         [<Button title = "dettagli" onPress = {NavigateGraph} />],
+         [<Button title = "dettagli" onPress = {NavigateGraph} />],
+         [<Button title = "dettagli" onPress = {NavigateGraph} />],
+
      ];
 
 
      return(
-
-        <View>
+         <View >
             <View style={styles.container}>
-                <Table>
-                    <Row data={tableData[0]} style={styles.head} textStyle={styles.text} />
-                    <Rows data={tableData.slice(1)} textStyle={styles.text} />
+                <Table style={{flexDirection: 'row'}} borderStyle={{borderWidth: 1}}>
+
+                    {/* Wrapper di sinistra*/}
+                    <TableWrapper style={{width: '80%', flexDirection: 'row'}}>
+                            <Col data={[1]} style={styles.head} heightArr={[200]} textStyle={styles.text} />
+                    </TableWrapper>
+
+                    {/* Wrapper di destra*/}
+                    <TableWrapper style={{width: '20%'}}>
+                        <Rows data={tableData} heightArr={[50, 50, 50, 50]} textStyle={styles.text}/>
+                    </TableWrapper>
+
                 </Table>
             </View>
 
-            <Picker
-                selectedValue={selectedOption}
-                onValueChange={(itemValue, itemIndex) =>
-                    setSelectedOption(itemValue)
-                }>
-                {options.map(option => (
-                    <Picker.Item
-                    key={option.value}
-                    label={option.label}
-                    value={option.value}
-                    />
-                ))}
-            </Picker>
-            <Button
-                title = "dettagli"
-                onPress = {NavigateGraph}
-            />
-            <Button
-                title = "aggiungi arnia"
-                onPress = {navigateAddHive}
-            />
-            <Switch
-                trackColor={{false: '#767577', true: '#81b0ff'}}
-                thumbColor={isEnabled ? '#ff00ff' : '#f0ff0f'}
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={toggleSwitch}
-                value={isEnabled}
-            />
-        </View>
-    )
+     <View >
+     <Picker
+         selectedValue={selectedOption}
+         onValueChange={(itemValue, itemIndex) =>
+             setSelectedOption(itemValue)
+         }>
+         {options.map(option => (
+             <Picker.Item
+                 key={option.value}
+                 label={option.label}
+                 value={option.value}
+             />
+         ))}
+     </Picker>
+     <Button
+         title = "dettagli"
+         onPress = {NavigateGraph}
+     />
+     <Button
+         title = "aggiungi arnia"
+         onPress = {navigateAddHive}
+     />
+     <Switch
+         trackColor={{false: '#767577', true: '#81b0ff'}}
+         thumbColor={isEnabled ? '#ff00ff' : '#f0ff0f'}
+         ios_backgroundColor="#3e3e3e"
+         onValueChange={toggleSwitch}
+         value={isEnabled}
+     />
+ </View>
+         </View>
+
+
+ )
 }
 
 const styles = StyleSheet.create({
     container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-    head: { height: 40, backgroundColor: '#f1f8ff' },
-    text: { margin: 6 , color: '#000000' }
+    celle: { height: 40, backgroundColor: '#F5F5F5' },
+    text: { margin: 6 , color: '#000000', textAlign: 'center' },
+        singleHead: { width: 80, height: 40, backgroundColor: '#c8e1ff' },
+        head: { flex: 1, backgroundColor: '#c8e1ff' },
+        title: { flex: 2.5, backgroundColor: '#f6f8fa' },
+        titleText: { marginRight: 6, textAlign:'right' },
+        btn: { width: 58, height: 18, marginLeft: 15, backgroundColor: '#c8e1ff', borderRadius: 2 },
+        btnText: { textAlign: 'center' }
 });
 
 export default Home;
